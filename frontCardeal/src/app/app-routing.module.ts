44d2fd7351b40d8920b20d87e984fa24f9376714
marcos-mv <vinicius_kof36@hotmail.com>
2021-09-14 17:31:44 -0300

    Adding Structure to Realtor registration.
    Co-authored-by: estevaoreis25  <estevaoreis1587@gmail.com>

--- frontCardeal/src/app/app-routing.module.ts
+++ frontCardeal/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { CrudCorretorComponent } from './views/crud-corretor/crud-corretor.component';
+
 
 const routes: Routes = [
   {
     path: '', 
     component: HomeComponent
+  },
+  {
+    path: 'cadastro-corretor', 
+    component: CrudCorretorComponent
   }
 ];
 
