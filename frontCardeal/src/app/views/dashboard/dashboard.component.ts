8095630c139aa3bea2d69db63dabc90db700d255
giovanadionisio <giovanadionisio.gd@gmail.com>
2021-09-16 14:18:37 -0300

    editing ONInit metods

--- frontCardeal/src/app/views/dashboard/dashboard.component.ts
+++ frontCardeal/src/app/views/dashboard/dashboard.component.ts
@@ -10,6 +10,10 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    document.querySelector("#link_home")!.classList.remove("ativo");
+    document.querySelector("#link_cadastro_imoveis")!.classList.remove("ativo");
+    document.querySelector("#link_faq")!.classList.remove("ativo");
+    document.querySelector("#link_busca_imoveis")!.classList.add("ativo");
   }
 
 }
