75c538abfe3d3c180ad8082a2ca9964635bc9d0e
marcos-mv <vinicius_kof36@hotmail.com>
2021-09-15 22:16:24 -0300

    Adding login component for the realtor.
    Co-authored-by: estevaoreis25  <estevaoreis1587@gmail.com>

---
+++ frontCardeal/src/app/views/login-corretor/login-corretor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-corretor',
+  templateUrl: './login-corretor.component.html',
+  styleUrls: ['./login-corretor.component.css']
+})
+export class LoginCorretorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
