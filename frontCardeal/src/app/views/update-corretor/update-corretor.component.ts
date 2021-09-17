ce3437bddc72618d79bdff3cde3524018235b174
marcos-mv <vinicius_kof36@hotmail.com>
2021-09-16 23:59:05 -0300

    Creating update component to property.
    Co-authored-by: tomas Veloso <tomasveloso@gmail.com>

---
+++ frontCardeal/src/app/views/update-corretor/update-corretor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-corretor',
+  templateUrl: './update-corretor.component.html',
+  styleUrls: ['./update-corretor.component.css']
+})
+export class UpdateCorretorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
