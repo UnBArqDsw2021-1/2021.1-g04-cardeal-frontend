7664bc36d418a1cacb0876360f0d575b2934d1d7
marcos-mv <vinicius_kof36@hotmail.com>
2021-09-17 08:52:19 -0300

    Adding Registration Owner page.

--- frontCardeal/src/app/views/cadastro-proprietario/cadastro-proprietario.component.ts
+++ frontCardeal/src/app/views/cadastro-proprietario/cadastro-proprietario.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroProprietarioComponent implements OnInit {
 
+  name!: string;
+  cpf!: string;
+  telephone!: string;
+  email!: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    document.querySelector("#link_home")!.classList.remove("ativo");
+    document.querySelector("#link_cadastro_imoveis")!.classList.remove("ativo");
+    document.querySelector("#link_faq")!.classList.remove("ativo");
+    document.querySelector("#link_busca_imoveis")!.classList.remove("ativo");
+  }
+
+  // constructor(private service: ProprietarioService, private route: Router){
+
+  // }
+
+  handlerSubmit(){
+    const realtor = {name:this.name,
+      cpf:this.cpf,
+      telephone:this.telephone,
+      email:this.email,
+      }
+    console.log(realtor)
+
+    // this.service.cadastraCorretor(realtor).subscribe(
+    //   resultado =>{
+    //     this.limparCampos();
+    //     this.route.navigateByUrl('login-corretor')
+    //   },
+    //   error => console.log(error)
+    // )
+  }
+
+  limparCampos(){
+    this.name = "";
+    this.cpf = "";
+    this.telephone = "";
+    this.email = "";
   }
 
 }
