2de377851584400fe911911d7db30ccb82821658
Tomás Veloso <tomasveloso@gmail.com>
2021-10-04 19:24:31 -0300

    fix bug

--- frontCardeal/src/app/views/meus-imoveis/meus-imoveis.component.ts
+++ frontCardeal/src/app/views/meus-imoveis/meus-imoveis.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { Imoveis, Imovel } from 'src/app/models/imovel.model';
-import ImovelService from 'src/app/services/imovel.service';
 
 @Component({
   selector: 'app-meus-imoveis',
